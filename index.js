439409fd89e42b0d65fc185447f40aca4599196e
brd3v <bnsiq2015@gmail.com>
2023-10-30 16:40:25 -0300

    mvp

--- index.js
+++ index.js
@@ -7,17 +7,34 @@ server.use(cors()); // Use o middleware cors para habilitar o CORS
 
 const estabelecimentos = [
     {
-        name: "Elton dinho",
-        tipo: 'restaurante'
+        name: "Ronchi Beer",
+        tipo: ['Cervejaria', 'Restaurante', "Bar"],
+        horario: "⏰ Seg a Sáb - 11h às 00h | Dom - 11h às 22h ❌",
+        descrição: 'A Ronchi Beer, fundada em 2018, é uma cervejaria que combina a produção artesanal de cervejas com uma culinária saborosa, incluindo pratos como joelho de porco. Localizada com vista para a deslumbrante Pedra Azul, ela oferece uma variedade de cervejas únicas, proporcionando aos visitantes uma experiência gastronômica memorável em um cenário magnifico.',
+        endereco: 'VALE DA PEDRA - Rua Nona Elvira P. Modolo, S/N, Pedra Azul, Domingos Martins 29278000',
+        instagram: 'https://www.instagram.com/ronchibeer/',
+        fotoFachada: 'https://i.ytimg.com/vi/ELex7hKjRCM/hqdefault.jpg'
     },
     {
-        name: "vere",
-        tipo: 'artesanato'
+        name: "Pousada Peterle",
+        tipo: ["Hotel"],
+        horario: "⏰ 24HRS ⏰",
+        descrição: 'A Pousada Peterle, uma das mais antigas e tradicionais opções de hospedagem na encantadora região de Pedra Azul, oferece uma experiência encantadora para seus hóspedes. Com sua história rica e acolhedora hospitalidade, a pousada é conhecida por suas acomodações charmosas e aconchegantes. Os visitantes podem desfrutar de quartos confortáveis, uma atmosfera tranquila e uma localização privilegiada, que proporciona vistas deslumbrantes para a icônica Pedra Azul. A pousada é o local ideal para aqueles que desejam escapar do ritmo agitado da cidade e se conectar com a natureza exuberante da região, enquanto desfrutam do atendimento personalizado e da tranquilidade que a Pousada Peterle oferece.',
+        endereco: 'Rodovia BR 262, km 88 S/n, Domingos Martins - ES, 29278-000',
+        instagram: 'https://www.instagram.com/pousadapeterleoficial/',
+        fotoFachada: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/63/21/b7/vista-panoramica-da-piscina.jpg'
     },
     {
-        name: "Parque estadual",
-        tipo: 'restaurante'
-    }
+        name: "Ronchi Beer",
+        tipo: ['Cervejaria', 'Restaurante', "Bar"],
+        horario: "⏰ Seg a Sáb - 11h às 00h | Dom - 11h às 22h ❌",
+        descrição: 'A Ronchi Beer, fundada em 2018, é uma cervejaria que combina a produção artesanal de cervejas com uma culinária saborosa, incluindo pratos como joelho de porco. Localizada com vista para a deslumbrante Pedra Azul, ela oferece uma variedade de cervejas únicas, proporcionando aos visitantes uma experiência gastronômica memorável em um cenário magnifico.',
+        endereco: 'VALE DA PEDRA - Rua Nona Elvira P. Modolo, S/N, Pedra Azul, Domingos Martins 29278000',
+        instagram: 'https://www.instagram.com/ronchibeer/',
+        fotoFachada: 'https://i.ytimg.com/vi/ELex7hKjRCM/hqdefault.jpg'
+    },
+
+   
 ]
 
 server.get('/estabelecimentos', (req, res) => {
