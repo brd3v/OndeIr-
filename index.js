c20c2d9fc32ab2f8cd0c0b7662b126250097ffdc
brd3v <bnsiq2015@gmail.com>
2023-10-30 23:06:22 -0300

    terminei o estabelecimento manualmente

--- index.js
+++ index.js
@@ -25,13 +25,13 @@ const estabelecimentos = [
         fotoFachada: 'https://images3.motor-reserva.com.br/curl/motor_reserva/images/configuracao_estabelecimento/cliente_2620/202201271643313195ohotel.jpg'
     },
     {
-        name: "Ronchi Beer",
-        tipo: ['Cervejaria', 'Restaurante', "Bar"],
-        horario: "⏰ Seg a Sáb - 11h às 00h | Dom - 11h às 22h ❌",
-        descrição: 'A Ronchi Beer, fundada em 2018, é uma cervejaria que combina a produção artesanal de cervejas com uma culinária saborosa, incluindo pratos como joelho de porco. Localizada com vista para a deslumbrante Pedra Azul, ela oferece uma variedade de cervejas únicas, proporcionando aos visitantes uma experiência gastronômica memorável em um cenário magnifico.',
-        endereco: 'VALE DA PEDRA - Rua Nona Elvira P. Modolo, S/N, Pedra Azul, Domingos Martins 29278000',
-        instagram: 'https://www.instagram.com/ronchibeer/',
-        fotoFachada: 'https://i.ytimg.com/vi/ELex7hKjRCM/hqdefault.jpg'
+        name: "Sitio Henraça",
+        tipo: ['Agroturismo'],
+        horario: "⏰ Ter a Dom - 08h às 17h  ❌",
+        descrição: 'O Sítio Herança de Pedra Azul é um encantador destino de agroturismo, imerso na beleza natural da região de Pedra Azul, no Brasil. Os visitantes podem desfrutar de uma experiência única de colheita de morangos, passear pelos campos verdejantes e pitorescos, e explorar as práticas agrícolas tradicionais, tudo em um ambiente tranquilo e relaxante. Com sua atmosfera serena e a oportunidade de se envolver em atividades agrícolas, o Sítio Herança de Pedra Azul oferece uma autêntica experiência de agroturismo para os amantes da natureza.',
+        endereco: 'Pedra Azul, Domingos Martins - ES, 29260-000',
+        instagram: 'https://www.instagram.com/sitioherancaagroturismo/',
+        fotoFachada: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/07/6d/64/69/sitio-heranca-agroturismo.jpg'
     },
 
    
