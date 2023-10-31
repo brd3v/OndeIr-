86cea65f6799677bf7a604828da8a3e39e48b8ad
brd3v <bnsiq2015@gmail.com>
2023-10-31 12:48:00 -0300

    tentative

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
-const express = require('express');
-const cors = require('cors'); // Importe a biblioteca cors
+import express from 'express';
+import cors from 'cors'; 
 const server = express();
+import mongoose from 'mongoose'
+import Estabelecimentos from './modules/estabelecimentos.js'
 
 server.use(express.json());
 server.use(cors()); // Use o middleware cors para habilitar o CORS
 
 const estabelecimentos = [
-    {
-        name: "Ronchi Beer",
-        tipo: ['Cervejaria', 'Restaurante', "Bar"],
-        horario: "⏰ Seg a Sáb - 11h às 00h | Dom - 11h às 22h ❌",
-        descrição: 'A Ronchi Beer, fundada em 2018, é uma cervejaria que combina a produção artesanal de cervejas com uma culinária saborosa, incluindo pratos como joelho de porco. Localizada com vista para a deslumbrante Pedra Azul, ela oferece uma variedade de cervejas únicas, proporcionando aos visitantes uma experiência gastronômica memorável em um cenário magnifico.',
-        endereco: 'VALE DA PEDRA - Rua Nona Elvira P. Modolo, S/N, Pedra Azul, Domingos Martins 29278000',
-        instagram: 'https://www.instagram.com/ronchibeer/',
-        fotoFachada: 'https://i.ytimg.com/vi/ELex7hKjRCM/hqdefault.jpg'
-    },
-    {
-        name: "Pousada Peterle",
-        tipo: ["Hotel"],
-        horario: "⏰ 24HRS ⏰",
-        descrição: 'A Pousada Peterle, uma das mais antigas e tradicionais opções de hospedagem na encantadora região de Pedra Azul, oferece uma experiência encantadora para seus hóspedes. Com sua história rica e acolhedora hospitalidade, a pousada é conhecida por suas acomodações charmosas e aconchegantes. Os visitantes podem desfrutar de quartos confortáveis, uma atmosfera tranquila e uma localização privilegiada, que proporciona vistas deslumbrantes para a icônica Pedra Azul. A pousada é o local ideal para aqueles que desejam escapar do ritmo agitado da cidade e se conectar com a natureza exuberante da região, enquanto desfrutam do atendimento personalizado e da tranquilidade que a Pousada Peterle oferece.',
-        endereco: 'Rodovia BR 262, km 88 S/n, Domingos Martins - ES, 29278-000',
-        instagram: 'https://www.instagram.com/pousadapeterleoficial/',
-        fotoFachada: 'https://images3.motor-reserva.com.br/curl/motor_reserva/images/configuracao_estabelecimento/cliente_2620/202201271643313195ohotel.jpg'
-    },
-    {
-        name: "Sitio Henraça",
-        tipo: ['Agroturismo'],
-        horario: "⏰ Ter a Dom - 08h às 17h  ❌",
-        descrição: 'O Sítio Herança de Pedra Azul é um encantador destino de agroturismo, imerso na beleza natural da região de Pedra Azul, no Brasil. Os visitantes podem desfrutar de uma experiência única de colheita de morangos, passear pelos campos verdejantes e pitorescos, e explorar as práticas agrícolas tradicionais, tudo em um ambiente tranquilo e relaxante. Com sua atmosfera serena e a oportunidade de se envolver em atividades agrícolas, o Sítio Herança de Pedra Azul oferece uma autêntica experiência de agroturismo para os amantes da natureza.',
-        endereco: 'Pedra Azul, Domingos Martins - ES, 29260-000',
-        instagram: 'https://www.instagram.com/sitioherancaagroturismo/',
-        fotoFachada: 'https://fastly.4sqi.net/img/general/width960/7960084_tQZvrocqoK2-XwfKFcb_AcnI3Pd8_y_KguJulixVx7A.jpg'
-    },
+  
 
    
 ]
@@ -41,11 +17,14 @@ server.get('/estabelecimentos', (req, res) => {
     return res.json(estabelecimentos);
 })
 
-server.post('/estabelecimentos', (req, res) => {
+server.post('/estabelecimentos', async (req, res) => {
         estabelecimentos.push(req.body)
         res.status(201).send('estabelecimento cadastrado com sucesso!')
 })
 
+mongoose.connect('mongodb+srv://brunim1233:outono123@cluster0.atlykqg.mongodb.net/?retryWrites=true&w=majority')
+.then(() => console.log('banco de dados conectado'))
+.catch(() => console.log('error'))
 server.listen(3000, () => {
     console.log('Servidor rodando em http://localhost:3000');
 });
