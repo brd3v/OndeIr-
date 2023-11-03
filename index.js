62d89c21cbb3367617aff59689afef2b04830e53
brd3v <bnsiq2015@gmail.com>
2023-11-03 20:05:58 -0300

    rescreevendo o codigo revertendo alterações

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
+const estabelecimentos = [
+    {
+        name: "Ronchi Beer",
+        tipo: ['Cervejaria', 'Restaurante', "Bar"],
+        horario: "⏰ Seg a Sáb - 11h às 00h | Dom - 11h às 22h ❌",
+        descrição: 'A Ronchi Beer, fundada em 2018, é uma cervejaria que combina a produção artesanal de cervejas com uma culinária saborosa, incluindo pratos como joelho de porco. Localizada com vista para a deslumbrante Pedra Azul, ela oferece uma variedade de cervejas únicas, proporcionando aos visitantes uma experiência gastronômica memorável em um cenário magnifico.',
+        endereco: 'VALE DA PEDRA - Rua Nona Elvira P. Modolo, S/N, Pedra Azul, Domingos Martins 29278000',
+        instagram: 'https://www.instagram.com/ronchibeer/',
+        fotoFachada: 'https://i.ytimg.com/vi/ELex7hKjRCM/hqdefault.jpg'
+    },
+    {
+        name: "Pousada Peterle",
+        tipo: ["Hotel"],
+        horario: "⏰ 24HRS ⏰",
+        descrição: 'A Pousada Peterle, uma das mais antigas e tradicionais opções de hospedagem na encantadora região de Pedra Azul, oferece uma experiência encantadora para seus hóspedes. Com sua história rica e acolhedora hospitalidade, a pousada é conhecida por suas acomodações charmosas e aconchegantes. Os visitantes podem desfrutar de quartos confortáveis, uma atmosfera tranquila e uma localização privilegiada, que proporciona vistas deslumbrantes para a icônica Pedra Azul. A pousada é o local ideal para aqueles que desejam escapar do ritmo agitado da cidade e se conectar com a natureza exuberante da região, enquanto desfrutam do atendimento personalizado e da tranquilidade que a Pousada Peterle oferece.',
+        endereco: 'Rodovia BR 262, km 88 S/n, Domingos Martins - ES, 29278-000',
+        instagram: 'https://www.instagram.com/pousadapeterleoficial/',
+        fotoFachada: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/63/21/b7/vista-panoramica-da-piscina.jpg'
+    },
+    {
+        name: "Ronchi Beer",
+        tipo: ['Cervejaria', 'Restaurante', "Bar"],
+        horario: "⏰ Seg a Sáb - 11h às 00h | Dom - 11h às 22h ❌",
+        descrição: 'A Ronchi Beer, fundada em 2018, é uma cervejaria que combina a produção artesanal de cervejas com uma culinária saborosa, incluindo pratos como joelho de porco. Localizada com vista para a deslumbrante Pedra Azul, ela oferece uma variedade de cervejas únicas, proporcionando aos visitantes uma experiência gastronômica memorável em um cenário magnifico.',
+        endereco: 'VALE DA PEDRA - Rua Nona Elvira P. Modolo, S/N, Pedra Azul, Domingos Martins 29278000',
+        instagram: 'https://www.instagram.com/ronchibeer/',
+        fotoFachada: 'https://i.ytimg.com/vi/ELex7hKjRCM/hqdefault.jpg'
+    },
 
-import  express  from 'express';
-import mongoose from 'mongoose'
-const bd = 'mongodb+srv://low:123@cluster0.oiezo0b.mongodb.net/?retryWrites=true&w=majority'
-const server = express();
-server.use(express.json());
-import Estabelecimentos from './models/estabelecimentos.js'
-server.get('/', async(request, response) => { 
-    const estabelecimentos = await Estabelecimentos.find() 
-    return response.json(estabelecimentos); 
+
+]
+
+server.get('/estabelecimentos', (req, res) => {
+    return res.json(estabelecimentos);
 })
-
-server.post("/estabelecimentos", async(request, response) => {
-    const estabelecimento = request.body
-    const newEstabelecimento = await Estabelecimentos.create(estabelecimento)
-    return response.json(newEstabelecimento)
-} )
-mongoose.connect(bd)
-.then(() => console.log('banco de dados online'))
-.catch(() => console.log('error'))
-server.listen(3000);
+server.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
+});
